a1742fa33e1f5c9bbe9f068b200cf53d06d93e10
MSI\aleo1 <aleodeleon5@gmail.com>
2023-05-29 18:14:54 +0800

    no message

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -21,6 +21,7 @@ const Header = ({ metadata = {}, noBlog = false }) => {
   const twitter = get(metadata, 'author', false);
   const github = get(metadata, 'github', false);
   const linkedin = get(metadata, 'linkedin', false);
+  const resume = get(metadata, 'linkedin', false)
 
   return (
     <div className={classes.wrapper}>
@@ -56,6 +57,13 @@ const Header = ({ metadata = {}, noBlog = false }) => {
             <li className={classes.item}>
               <a className={classes.link} href={linkedin}>
                 LinkedIn
+              </a>
+            </li>
+          )}
+		   {resume && (
+            <li className={classes.item}>
+              <a className={classes.link} href={resume}>
+                Resume
               </a>
             </li>
           )}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -47,6 +47,7 @@ export const pageQuery = graphql`
         author
         github
         linkedin
+		resume
         projects {
           name
           description
